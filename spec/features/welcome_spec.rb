3ff7b1eab02f3c8ae69c640e6b2ccfe978b7a121
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-09 13:33:18 -0600

    Formatted money is displayed on homepage
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -4,7 +4,7 @@ feature "Homepage" do
   scenario "It displays a welcome message on homepage" do
     item = Product.create_from_sku("B0047Y6I24")
     visit '/'
-    expect(page).to have_content(item.current_price)
+    expect(page).to have_content(ProductCurrency.format_money(item.current_price))
     within "img" do
       expect(page).to have_xpath("//img[@src=\"#{item.large_image_url}\"]")
     end
