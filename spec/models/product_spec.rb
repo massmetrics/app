cf7ed6aab9cb8f0d12e61ff7f09219f9f82843ea
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-09 11:21:11 -0600

    Validates item is valid after creation
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,7 @@ describe Product do
   it 'can add a new product from an SKU - Myotein Chocolate' do
     item = Product.create_from_sku("B0047Y6I24")
     expect(item.sku).to eq("B0047Y6I24")
+    expect(item.valid?).to eq(true)
   end
 
   it 'wont create an item if sku is already in db' do
