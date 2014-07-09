d84730568c9370bf18f9c1f82e142b6de21ebaa7
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-09 13:25:13 -0600

    Front page has products and money is formatted
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 describe Product do
+  before do
+    @item = Product.create_from_sku("B0047Y6I24")
+  end
   it 'can add a new product from an SKU - Myotein Chocolate' do
-    item = Product.create_from_sku("B0047Y6I24")
-    expect(item.sku).to eq("B0047Y6I24")
-    expect(item.valid?).to eq(true)
+    expect(@item.sku).to eq("B0047Y6I24")
+    expect(@item.valid?).to eq(true)
   end
 
   it 'wont create an item if sku is already in db' do
-    Product.create_from_sku("B0047Y6I24")
     invalid_item = Product.create_from_sku("B0047Y6I24")
     expect(invalid_item.valid?).to eq(false)
   end
