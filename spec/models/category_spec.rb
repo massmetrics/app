351c7f8ef8bdb5f0c3779aaee6fa524885806487
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-15 11:44:55 -0600

    Can fetch a list of all categories
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe Category do
+  it "returns a list of all categories" do
+    Category.create(category: "Protein")
+    Category.create(category: "Pre workout")
+    Category.create(category: "Pre workout")
+
+    expect(Category.category_list).to match_array(["Protein", "Pre workout"])
+  end
+end
