efe7ff8880acd8148f299ce93136f4b79a15e8c2
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-16 11:45:53 -0600

    User can click on a category and see a list of products
    Signed-off-by: Steve Magelowitz <steven.magelowitz@gmail.com>

--- spec/features/category_spec.rb
+++ spec/features/category_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 feature 'homepage' do
   before do
-   @category_1 = Category.create(category: 'Protein')
-   @category_2 = Category.create(category: 'Pre workout')
+    @category_1 = ObjectCreation.create_product_with_category({category: 'Protein'})
+    @product = Product.last
+    @category_2 = Category.create(category: 'Pre workout')
   end
   scenario 'has a link to each category' do
     visit '/'
     click_link 'Browse'
     expect(page).to have_content @category_1.category
     expect(page).to have_content @category_2.category
+    click_on @category_1.category
+    expect(page).to have_content @product.title
+
   end
 end
