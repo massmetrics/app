d8663eb9597f97e50ef0cebbbb160dd992e6338b
Evan Tedesco <evantedesco@gmail.com>
2014-07-16 11:45:53 -0600

    User can click on a category and see a list of products
    Signed-off-by: Steve Magelowitz <steven.magelowitz@gmail.com>

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,4 +3,9 @@ class CategoryController < ApplicationController
   def index
     @categories = Category.category_list
   end
+
+  def show
+    @products = Product.get_products_for(params[:id])
+  end
+
 end
