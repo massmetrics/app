b16fccfcadf554cd2907a4e8ca20f9b2c87f2345
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-17 16:07:30 -0600

    it removes $ and . from price string
    Signed-off-by: Steve Magelowitz <steven.magelowitz@gmail.com>

--- spec/models/number_formatter_spec.rb
+++ spec/models/number_formatter_spec.rb
@@ -6,4 +6,10 @@ describe NumberFormatter do
 
     expect(NumberFormatter.format_percentage(percentage)).to eq(["33.33%", 33.33])
   end
+
+  it 'strips $ and . from a string' do
+    amount = '$17.95'
+
+    expect(NumberFormatter.format_price_string(amount)).to eq('1795')
+  end
 end
