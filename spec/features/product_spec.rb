533706ed1e3f7ff9d42110c91e8c11af422e7265
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-18 19:47:01 -0600

    added spacing for test readability

--- spec/features/product_spec.rb
+++ spec/features/product_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 feature "Product" do
   scenario "User can view product details" do
     product = ObjectCreation.create_product
+
     visit '/'
     click_link "#title"
     expect(page).to have_content product.title
     expect(page).to have_content(ProductCurrency.format_money(product.current_price))
+
     within "img" do
       expect(page).to have_xpath("//img[@src=\"#{product.large_image_url}\"]")
     end
