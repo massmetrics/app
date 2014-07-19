267a708a14761e626898c55475078c74e46edacd
Evan Tedesco <evantedesco@gmail.com>
2014-07-18 19:45:08 -0600

    removed unused before block

--- spec/features/category_spec.rb
+++ spec/features/category_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 feature 'homepage' do
-  before do
-    @category_1 = ObjectCreation.create_product_with_category({category: 'Protein'})
-    @product = Product.find_by_sku('12345')
-    ObjectCreation.create_price_log(product: @product)
-    @category_2 = Category.create(category: 'Pre workout')
-  end
   scenario 'has a link to each category' do
+    category_1 = ObjectCreation.create_product_with_category({category: 'Protein'})
+    product = Product.find_by_sku('12345')
+    ObjectCreation.create_price_log(product: product)
+    category_2 = Category.create(category: 'Pre workout')
+
     visit '/'
     click_link 'Browse'
-    expect(page).to have_content @category_1.category
-    expect(page).to have_content @category_2.category
-    click_on @category_1.category
-    # binding.pry
-    expect(page).to have_content @product.title
+
+    expect(page).to have_content category_1.category
+    expect(page).to have_content category_2.category
+
+    click_on category_1.category
+
+    expect(page).to have_content product.title
   end
 end
