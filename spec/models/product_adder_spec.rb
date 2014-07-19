955f0bb6b6e679e0020683d346d52e64d7ed8192
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-19 17:32:14 -0600

    Updated spec time

--- spec/models/product_adder_spec.rb
+++ spec/models/product_adder_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe ProductAdder do
   it 'Adds a product and a category at the same time' do
-    new_time = "2014-07-19T15:52:41"
+    new_time = "2014-07-19T21:48:14Z"
     Timecop.freeze(new_time) do
       VCR.use_cassette('/models/product_adder/B000QSNYGI') do
         product = ProductAdder.add("B000QSNYGI", ["Protein"])
