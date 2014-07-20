bf51badb9dff05b713380020d562516f1c92be59
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-20 14:42:31 -0600

    Now checks for sales price if list price doesnt exist

--- app/models/amazon_scraper.rb
+++ app/models/amazon_scraper.rb
@@ -6,6 +6,10 @@ class AmazonScraper
   end
 
   def price
-    @page.xpath('//span[@id = "priceblock_ourprice"]').first.children.to_s
+    if @page.xpath('//span[@id = "priceblock_ourprice"]').first
+      @page.xpath('//span[@id = "priceblock_ourprice"]').first.children.to_s
+    elsif @page.xpath('//span[@id = "priceblock_saleprice"]').first
+      @page.xpath('//span[@id = "priceblock_saleprice"]').first.children.to_s
+    end
   end
 end
