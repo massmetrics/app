2aa28477f7d5d83b8f33b2c149d1aac00c48a15d
Steve Magelowitz <steven.magelowitz@gmail.com>
2014-07-20 15:12:48 -0600

    category list only returns categories which have products and pricelogs
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 describe Category do
   it 'returns a list of all categories' do
-    Category.create(category: 'Protein')
-    Category.create(category: 'Pre workout')
-    Category.create(category: 'Pre workout')
+    ObjectCreation.create_product_with_category({category: 'Protein'})
+    product = Product.find_by_sku('12345')
+    ObjectCreation.create_price_log(product: product)
+    ObjectCreation.create_product_with_category({category: 'Pre-workout'}, {sku: 'product2'})
+    product2 = Product.find_by_sku('product2')
+    ObjectCreation.create_price_log(product: product2)
+    ObjectCreation.create_product_with_category({category: 'Pre-workout'}, {sku: 'product3'})
+    product3 = Product.find_by_sku('product3')
+    ObjectCreation.create_price_log(product: product3)
 
-    expect(Category.category_list).to match_array(['Protein', 'Pre workout'])
+
+    expect(Category.category_list).to match_array(['Protein', 'Pre-workout'])
   end
 end
