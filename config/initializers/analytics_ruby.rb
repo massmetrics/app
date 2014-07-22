ad556d611d02aa03ae82809dc3cd988dc0d6c9fe
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-22 14:39:25 -0600

    Updated segment code

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,5 +1,4 @@
-Analytics = AnalyticsRuby       # Alias for convenience
-Analytics.init({
-                 secret: ENV['SEGMENT'],          # The write key for stevenmagelowitz/massmetrics
-                 on_error: Proc.new { |status, msg| print msg }  # Optional error handler
-               })
+Analytics = Segment::Analytics.new({
+                                     write_key: ENV['SEGMENT'],
+                                     on_error: Proc.new { |status, msg| print msg }
+                                   })
