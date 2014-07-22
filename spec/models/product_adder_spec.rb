e1922e3d3d05c736a097bef6827cc80153b0aa71
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-22 08:56:47 -0600

    normalized spacing and quotes in accordance with style guide

--- spec/models/product_adder_spec.rb
+++ spec/models/product_adder_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 describe ProductAdder do
   it 'Adds a product and a category at the same time' do
-    new_time = "2014-07-19T21:48:14Z"
+    new_time = '2014-07-19T21:48:14Z'
     Timecop.freeze(new_time) do
       VCR.use_cassette('/models/product_adder/B000QSNYGI') do
-        product = ProductAdder.add("B000QSNYGI", ["Protein"])
-        expect(product.current_price).to eq("5799")
-        expect(product.categories.first.category).to eq("Protein")
+        product = ProductAdder.add('B000QSNYGI', ['Protein'])
+        
+        expect(product.current_price).to eq('5799')
+        expect(product.categories.first.category).to eq('Protein')
       end
     end
   end
