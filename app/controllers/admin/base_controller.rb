645b689bcf17e00c8e28855de30bb167d0a59327
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 16:00:44 -0600

    Added role column
    check for admin before allowing users to go to the admin section
    
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,9 +2,13 @@ module Admin
   class BaseController < ApplicationController
     before_filter :verify_admin
 
+    def index
+
+    end
+
     private
     def verify_admin
-      redirect_to root_path, notice: "You don't have permission to access that page" unless logged_in? && current_user.try(current_user.role?('admin'))
+      redirect_to root_path, notice: "You don't have permission to access that page" unless logged_in? && current_user.role?(:admin)
     end
   end
 end
