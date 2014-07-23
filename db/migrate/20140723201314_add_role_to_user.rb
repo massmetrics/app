ed39a792e8b1b838b20be256c2fe25a0867ddeeb
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 14:56:19 -0600

    changed user role to be enumerable and validate uniqueness of email
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- db/migrate/20140723201314_add_role_to_user.rb
+++ db/migrate/20140723201314_add_role_to_user.rb
@@ -1,5 +1,5 @@
 class AddRoleToUser < ActiveRecord::Migration
   def change
-    add_column :users, :role, :string, default: 'user'
+    add_column :users, :role, :integer
   end
 end
--- spec/features/permissions_spec.rb
+++ spec/features/permissions_spec.rb
@@ -28,4 +28,20 @@ feature 'Permissions and authorization' do
       expect(page).to have_content("You don't have permission to access that page")
     end
   end
+
+  context 'admin' do
+    before do
+      user = ObjectCreation.create_user(role: 'admin')
+      visit '/'
+      click_link 'Login'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'password'
+      click_button 'Login'
+    end
+    scenario 'viewing suggested products' do
+      expect(page).to_not have_link('Admin')
+      visit admin_submissions_path
+      expect(page).to have_content("You don't have permission to access that page")
+    end
+  end
 end
