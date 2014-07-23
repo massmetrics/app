be5d2e3b61c3364dec08be6a94c7d518b4138db4
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 14:43:08 -0600

    Logged in users cannot visit admin section
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- spec/features/permissions_spec.rb
+++ spec/features/permissions_spec.rb
@@ -4,9 +4,25 @@ feature 'Permissions and authorization' do
   before do
     Submission.create!(sku: '12345', category: 'Some category')
   end
-  context 'user' do
+  context 'guest' do
     scenario 'viewing the suggested products' do
       visit '/'
+      expect(page).to_not have_link('Admin')
+      visit admin_submissions_path
+      expect(page).to have_content("You don't have permission to access that page")
+    end
+  end
+
+  context 'user' do
+    before do
+      user = ObjectCreation.create_user
+      visit '/'
+      click_link 'Login'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'password'
+      click_button 'Login'
+    end
+    scenario 'viewing suggested products' do
       expect(page).to_not have_link('Admin')
       visit admin_submissions_path
       expect(page).to have_content("You don't have permission to access that page")
