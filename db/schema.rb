e678f238b395132c33a66c5f2e31834ed1b8303d
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 14:56:19 -0600

    changed user role to be enumerable and validate uniqueness of email
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -60,9 +60,9 @@ ActiveRecord::Schema.define(version: 20140723201314) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                                            null: false
-    t.string   "crypted_password",                                 null: false
-    t.string   "salt",                                             null: false
+    t.string   "email",                           null: false
+    t.string   "crypted_password",                null: false
+    t.string   "salt",                            null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_me_token"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20140723201314) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
-    t.string   "role",                            default: "user"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,15 @@ describe User do
       expect(invalid_user).to be_invalid
     end
 
+    it 'validates uniqueness of email' do
+      valid_user = User.create(email: 'joe@example.com', password: 'password', password_confirmation: 'password')
+      invalid_user = User.new(email: 'joe@example.com', password: 'password', password_confirmation: 'password confirmation')
+
+      expect(valid_user).to be_valid
+      expect(invalid_user).to be_invalid
+
+    end
+
     it 'validates presence of password' do
       valid_user = User.new(email: 'joe@example.com', password: 'password', password_confirmation: 'password')
       invalid_user = User.new(email: 'sue@example.com', password_confirmation: 'password confirmation')
@@ -26,7 +35,7 @@ describe User do
       expect(invalid_user).to be_invalid
     end
 
-    it 'validates uniqueness of email' do
+    it 'validates password and password confirmation match' do
       valid_user = User.new(email: 'joe@example.com', password: 'password', password_confirmation: 'password')
       invalid_user = User.new(email:'joe@example.com', password: 'password', password_confirmation: 'password confirmation')
 
@@ -36,15 +45,15 @@ describe User do
   end
 
   it 'defaults role to user' do
-    user = User.new(email: 'joe@example.com', password: 'password', password_confirmation: 'password')
+    user = User.create(email: 'joe@example.com', password: 'password', password_confirmation: 'password')
     expect(user.role).to eq('user')
   end
 
   it 'checks the users role' do
-    user = User.new(email: 'joe@example.com', password: 'password', password_confirmation: 'password')
+    user = User.create(email: 'joe@example.com', password: 'password', password_confirmation: 'password')
     expect(user.role?('admin')).to eq(false)
     expect(user.role?('user')).to eq(true)
-    admin_user = User.new(email: 'joe@example.com', password: 'password', password_confirmation: 'password', role: 'admin')
+    admin_user = User.create(email: 'jon@example.com', password: 'password', password_confirmation: 'password', role: 'admin')
     expect(admin_user.role?('admin')).to eq(true)
     expect(admin_user.role?('user')).to eq(false)
   end
