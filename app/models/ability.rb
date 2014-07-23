23640eeed6c0c4a689ca1af76e3f4f54bcc280b9
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 14:32:27 -0600

    Non logged in users cannot access the admin section
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+
+    can :read, :all
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
