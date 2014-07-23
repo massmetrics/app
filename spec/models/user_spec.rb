e0923a61312d2503f5e22c9e69d57b7fdc8cbecb
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-22 18:06:27 -0600

    it validates presence of password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,16 @@ describe User do
       expect(valid_user).to be_valid
       expect(invalid_user).to be_invalid
     end
+
+    it 'validates presence of password' do
+      valid_user = User.new(email: 'joe@example.com', password: 'password', password_confirmation: 'password')
+      invalid_user = User.new(email: 'sue@example.com', password_confirmation: 'password confirmation')
+
+      expect(valid_user).to be_valid
+      expect(invalid_user).to be_invalid
+    end
+
+    
   end
 end
 
