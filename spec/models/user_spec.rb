409822bebfb861d32d8285306ec27179e107b2d5
Evan Tedesco <evantedesco@gmail.com>
2014-07-22 18:07:36 -0600

    it validates presence of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,7 +18,13 @@ describe User do
       expect(invalid_user).to be_invalid
     end
 
-    
+    it 'validates presence of email' do
+      valid_user = User.new(email: 'joe@example.com', password: 'password', password_confirmation: 'password')
+      invalid_user = User.new( password: 'password', password_confirmation: 'password confirmation')
+
+      expect(valid_user).to be_valid
+      expect(invalid_user).to be_invalid
+    end
   end
 end
 
