04f27fa344575a4356fd1f4a5eadecbb475e9f2f
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 21:15:39 -0600

    it validates length of submissions

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 describe Submission do
   it 'requires both an sku and a category' do
-    protein = Submission.new(sku: '12345')
+    protein = Submission.new(sku: 'B0031JK96C')
+    protein2 = Submission.new(category: 'test')
     expect(protein.valid?).to eq(false)
     expect(protein.errors.full_messages).to eq(["Category can't be blank"])
-    protein.sku = nil
-    protein.category = "test"
-    expect(protein.valid?).to eq(false)
-    expect(protein.errors.full_messages).to eq(["Sku can't be blank"])
-    protein.sku = "12345"
-    expect(protein.valid?).to eq(true)
+    expect(protein2.valid?).to eq(false)
+    expect(protein2.errors.full_messages).to eq(["Sku can't be blank", "Sku is the wrong length (should be 10 characters)"])
+  end
+
+  context 'validations' do
+    it 'validates length of sku' do
+      valid_submission = Submission.new(sku: 'B0031JK96C', category: 'Protein')
+      invalid_submission = Submission.new(sku: 'B', category: 'Protein')
+
+      expect(valid_submission.valid?).to eq(true)
+      expect(invalid_submission.valid?).to eq(false)
+    end
   end
 end
