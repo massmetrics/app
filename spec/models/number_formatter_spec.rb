1f002f18c8e137660e2143c26e132325ee6e2897
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 21:45:28 -0600

    normalized quotes in test suite

--- spec/models/number_formatter_spec.rb
+++ spec/models/number_formatter_spec.rb
@@ -4,7 +4,7 @@ describe NumberFormatter do
   it 'formats percentages for displaying purposes' do
     percentage = 0.3333333333333
 
-    expect(NumberFormatter.format_percentage(percentage)).to eq(["33.33%", 33.33])
+    expect(NumberFormatter.format_percentage(percentage)).to eq(['33.33%', 33.33])
   end
 
   it 'strips $ and . from a string' do
