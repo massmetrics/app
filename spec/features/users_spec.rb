03b23375c8072cef1f0a42a3288ec57b53810163
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 22:43:48 -0600

    added test for registration failure case

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -10,8 +10,20 @@ feature 'Sessions' do
     click_button 'Register'
 
     expect(page).to have_content 'Joe@example.com'
-    expect(page).to have_link 'Logout'
+    expect(page).to have_link('Logout')
     expect(page).to_not have_link('Register')
     expect(page).to_not have_link('Login')
+  end
+
+  scenario 'A user is redirected to register page if registration fails' do
+    visit '/'
+    click_link 'Register'
+    fill_in 'Email', with: 'n'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Register'
+
+    expect(page).to have_link('Register')
+    expect(page).to have_link('Login')
   end
 end
