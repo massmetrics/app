a709ff2416bc8515e70a3caf9e7b743e9dffc19f
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 18:07:23 -0600

    Admin can accecpt new product suggestions
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- app/controllers/admin/submissions_controller.rb
+++ app/controllers/admin/submissions_controller.rb
@@ -4,5 +4,22 @@ module Admin
     def index
       @submissions = Submission.all
     end
+
+    def edit
+      @submission = Submission.find(params[:id])
+      @amazon_detail_page = "http://www.amazon.com/gp/product/#{@submission.sku}/"
+    end
+
+    def update
+      categories = SubmissionHelper.split_categories(params[:submission][:category])
+      sku = params[:submission][:sku]
+      ProductAdder.add([sku], categories)
+      redirect_to admin_submissions_path
+    end
+
+    private
+    def allowed_params
+      params.require(:submission).permit(:sku, :category)
+    end
   end
 end
