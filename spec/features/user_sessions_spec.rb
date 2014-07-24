9619f0ae1ca859edc13e51d2d664e9a74895cf38
Evan Tedesco <evantedesco@gmail.com>
2014-07-23 22:35:21 -0600

    added test for failed login

--- spec/features/user_sessions_spec.rb
+++ spec/features/user_sessions_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 feature 'Sessions' do
+  before do
+    @user = ObjectCreation.create_user
+
+  end
   scenario 'User can login and logout' do
-    user = ObjectCreation.create_user
 
     visit '/'
     click_link 'Login'
-    fill_in 'Email', with: user.email
+    fill_in 'Email', with: @user.email
     fill_in 'Password', with: 'password'
     click_button 'Login'
 
-    expect(page).to have_content("#{user.email}")
+    expect(page).to have_content("#{@user.email}")
     expect(page).to have_link('Logout')
     expect(page).to_not have_link('Register')
     expect(page).to_not have_link('Login')
@@ -20,6 +23,16 @@ feature 'Sessions' do
     expect(page).to have_link('Login')
     expect(page).to have_link('Register')
     expect(page).to_not have_content('Logout')
-    expect(page).to_not have_content("#{user.email}")
+    expect(page).to_not have_content("#{@user.email}")
+  end
+
+  scenario 'User is given an error if login fails' do
+    visit '/'
+    click_link 'Login'
+    fill_in 'Email', with: 'incorrectemail@noplace.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Login'
+
+    expect(page).to have_content('Login failed')
   end
 end
