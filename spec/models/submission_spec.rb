251630b1ec715adff502f6a7df4da37ab9201751
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 21:23:23 -0600

    seperated validations for ease of debugging

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -1,15 +1,6 @@
 require 'rails_helper'
 
 describe Submission do
-  it 'requires both an sku and a category' do
-    protein = Submission.new(sku: 'B0031JK96C')
-    protein2 = Submission.new(category: 'test')
-    expect(protein.valid?).to eq(false)
-    expect(protein.errors.full_messages).to eq(["Category can't be blank"])
-    expect(protein2.valid?).to eq(false)
-    expect(protein2.errors.full_messages).to eq(["Sku can't be blank", "Sku is the wrong length (should be 10 characters)"])
-  end
-
   context 'validations' do
     it 'validates length of sku' do
       valid_submission = Submission.new(sku: 'B0031JK96C', category: 'Protein')
@@ -17,6 +8,24 @@ describe Submission do
 
       expect(valid_submission.valid?).to eq(true)
       expect(invalid_submission.valid?).to eq(false)
+    end
+
+    it 'validates presence of sku' do
+      valid_submission = Submission.new(sku: 'B0031JK96C', category: 'Protein')
+      invalid_submission = Submission.new(category: 'Protein')
+
+      expect(valid_submission.valid?).to eq(true)
+      expect(invalid_submission.valid?).to eq(false)
+      expect(invalid_submission.errors.full_messages).to eq(["Sku can't be blank", "Sku is the wrong length (should be 10 characters)"])
+    end
+
+    it 'validates presence of category' do
+      valid_submission = Submission.new(sku: 'B0031JK96C', category: 'Protein')
+      invalid_submission = Submission.new(sku: 'B0031JK96C')
+
+      expect(valid_submission.valid?).to eq(true)
+      expect(invalid_submission.valid?).to eq(false)
+      expect(invalid_submission.errors.full_messages).to eq(["Category can't be blank"])
     end
   end
 end
