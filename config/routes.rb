f91c1ccf3f202c59cdc4285bd2834937e28b0abb
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-23 18:25:55 -0600

    Refactored the routes
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'category#index'
   resources :welcome, only: [:index]
   resources :product, only: [:show]
-  resources :category
-  resources :submissions
-  resources :users
-  resources :user_sessions
+  resources :category, only: [:index, :show]
+  resources :submissions, only: [:new, :create]
+  resources :users, only: [:new, :create]
+  resources :user_sessions, only: [:new, :create, :destroy]
   namespace :admin do
-    resources :base
-    resources :submissions
+    resources :base, only: [:index]
+    resources :submissions, only: [:index, :edit, :update, :destroy]
   end
 end
