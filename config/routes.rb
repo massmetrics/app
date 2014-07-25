da7aac8cb3dc3f5b938a3b1e4f3fca450ab6b924
Evan Tedesco <evantedesco@gmail.com>
2014-07-24 18:47:25 -0600

    Users can track products
    guests must log in to track products
    
    Signed-off-by: Steven Magelowitz <steven.magelowitz@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'category#index'
   resources :welcome, only: [:index]
-  resources :products, only: [:show]
+  resources :products, only: [:show] do
+    resources :my_products, only: [:create]
+  end
   resources :category, only: [:index, :show]
   resources :submissions, only: [:new, :create]
   resources :users, only: [:new, :create, :show]
