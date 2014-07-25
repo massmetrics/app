ed3adee29377eaa604b8588e894f0e202874c3e7
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-25 11:50:17 -0600

    User can delete tracked items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'category#index'
   resources :welcome, only: [:index]
   resources :products, only: [:show] do
-    resources :my_products, only: [:create]
+    resources :my_products, only: [:create, :destroy]
   end
   resources :category, only: [:index, :show]
   resources :submissions, only: [:new, :create]
