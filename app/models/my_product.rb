8a83cd1975912f7eb25a25c628a3850e8e394bb5
Steve Magelowitz <steven.magelowitz@gmail.com>
2014-07-25 16:28:01 -0600

    user can enter a percentage discount threshold
    Signed-off-by: Evan Tedesco <evantedesco@gmail.com>

--- app/models/my_product.rb
+++ app/models/my_product.rb
@@ -1,7 +1,6 @@
 class MyProduct < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
-
+  has_one :my_products_notification
   validates_uniqueness_of :product, scope: :user, message: 'is already tracked'
-
 end
--- spec/features/user/my_products_spec.rb
+++ spec/features/user/my_products_spec.rb
@@ -7,10 +7,25 @@ feature 'My Products' do
       @product = ObjectCreation.create_product
       MyProduct.create(user: @user, product: @product)
     end
+
     scenario 'User removes a product that they tracked' do
       visit user_path(@user)
       click_on 'Untrack'
       expect(page).to_not have_content(@product.title)
+    end
+
+    scenario 'User adds pricing threshold' do
+      visit user_path(@user)
+      fill_in 'my_products_notification[discount]', with: '15'
+      click_on 'Save notification'
+      expect(find_field('my_products_notification[discount]').value).to eq('15.0')
+    end
+
+    scenario 'User adds invalid discount' do
+      visit user_path(@user)
+      fill_in 'my_products_notification[discount]', with: '15asdf%'
+      click_on 'Save notification'
+      expect(page).to have_content('Invalid format for discount')
     end
   end
 end
