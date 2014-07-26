1d37d2f59d46d4aa6acb4b5a758a3262d205454b
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-26 14:42:09 -0600

    Can check if a user is tracking a product

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,13 @@ class User < ActiveRecord::Base
     self.role == role
   end
 
+  def tracked?(product)
+    my_products.each do |my_product|
+      return my_product.product == product
+    end
+    false
+  end
+
   private
   def set_role
     self.role = :user unless self.role
