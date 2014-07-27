9c2e716651f89800d1da4d264c8ed221015919ff
Steve Magelowitz <steven.magelowitz@gmail.com>
2014-07-26 23:49:44 -0600

    test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ CodeClimate::TestReporter.start
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require 'capybara'
+require 'sucker_punch/testing/inline'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
