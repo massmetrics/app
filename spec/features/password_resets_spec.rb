ada9ed4b20990eda63f673aaae01011e4757640d
Evan Tedesco <evantedesco@gmail.com>
2014-07-28 21:24:45 -0600

    user is redirected to root if blank

--- spec/features/password_resets_spec.rb
+++ spec/features/password_resets_spec.rb
@@ -45,4 +45,22 @@ feature 'forgot password' do
     expect(page).to have_content 'Password confirmation doesn\'t match Password'
   end
 
+  scenario 'user is redirected to root if blank' do
+    user = ObjectCreation.create_user(reset_password_token: '10')
+
+    visit new_user_session_path
+    click_on 'Forgot Password?'
+    fill_in 'Email', with: user.email
+    click_on 'Reset Password'
+
+    expect(page).to have_content 'Instructions have been sent to your email.'
+    user.reload
+    visit edit_password_reset_path(user.reset_password_token)
+    User.destroy(user.id)
+    fill_in 'user[password]', with: 'new_password'
+    fill_in 'user[password_confirmation]', with: 'new_password'
+    click_on 'Reset Password'
+
+    expect(page).to have_content 'Best deals'
+  end
 end
