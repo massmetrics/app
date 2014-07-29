cc4c25850f25ee928f4dc4ed9decdbccd4bf8bf4
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-28 21:13:52 -0600

    user can see an error message if pw doesn't match confirmation'

--- spec/features/password_resets_spec.rb
+++ spec/features/password_resets_spec.rb
@@ -25,4 +25,24 @@ feature 'forgot password' do
     click_button 'Login'
     expect(page).to have_content(user.email)
   end
+
+  scenario 'user is redirected to edit page if password and confirmation differ' do
+    user = ObjectCreation.create_user(reset_password_token: '10')
+
+    visit new_user_session_path
+    click_on 'Forgot Password?'
+    fill_in 'Email', with: user.email
+    click_on 'Reset Password'
+
+    expect(page).to have_content 'Instructions have been sent to your email.'
+    user.reload
+    visit edit_password_reset_path(user.reset_password_token)
+
+    fill_in 'user[password]', with: 'new_password'
+    fill_in 'user[password_confirmation]', with: 'password'
+    click_on 'Reset Password'
+
+    expect(page).to have_content 'Password confirmation doesn\'t match Password'
+  end
+
 end
