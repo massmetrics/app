016f1badceff321923343f3031e941dec99bc1be
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-29 19:49:24 -0600

    changed test to look for error message

--- spec/features/user/my_products_spec.rb
+++ spec/features/user/my_products_spec.rb
@@ -39,13 +39,11 @@ feature 'My Products' do
   end
 
   scenario 'User is redirected back if he doesn\'t exist' do
-    user = FeatureSupport.create_and_login_user
-    product = ObjectCreation.create_product
-    category = ObjectCreation.create_category(product: product)
-    User.destroy(user.id)
+    ObjectCreation.create_product
 
-    visit category_path(category)
-    
-    expect(page).to have_no_link('Track-it')
+    visit root_path
+    click_on 'Track-it'
+
+    expect(page).to have_content 'You must log in to track products'
   end
 end
