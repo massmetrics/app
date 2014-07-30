b4eb694aac6c729057a229759f64b3c182653c48
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-30 14:56:55 -0600

    Add self class to product adder

--- app/models/product_adder.rb
+++ app/models/product_adder.rb
@@ -1,9 +1,11 @@
 class ProductAdder
-  def self.add(sku_array, category_array)
-    sku_array.each do |sku|
-      product = Product.find_by_sku(sku) || Product.create_from_sku(sku)
-      if product
-        product.add_categories(category_array)
+  class << self
+    def add(sku_array, category_array)
+      sku_array.each do |sku|
+        product = Product.find_by_sku(sku) || Product.create_from_sku(sku)
+        if product
+          product.add_categories(category_array)
+        end
       end
     end
   end
