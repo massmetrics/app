238585420426082d3ca97dbb232410d4f9c16989
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-07-29 20:05:26 -0600

    test for 100% discount and removed unused controller action

--- app/models/my_products_notification.rb
+++ app/models/my_products_notification.rb
@@ -2,7 +2,7 @@ class MyProductsNotification < ActiveRecord::Base
   belongs_to :my_product
   has_one :user, through: :my_product
   before_create :string_to_float
-  validates :discount, numericality: { greater_than: 0, less_than_or_equal_to: 100, message: 'is invalid.' }
+  validates :discount, numericality: { greater_than: 0, less_than: 100, message: 'is invalid.' }
 
 
   private
