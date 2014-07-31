775e87f12c229c16282d7396aa2a68bc3b790e15
Steve Magelowitz <steven.magelowitz@gmail.com>
2014-07-31 16:34:55 -0600

    removed notification date from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140731170314) do
+ActiveRecord::Schema.define(version: 20140731223204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,7 +87,6 @@ ActiveRecord::Schema.define(version: 20140731170314) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer  "role_cd"
-    t.datetime "notification_date"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
