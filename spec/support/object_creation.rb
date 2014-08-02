baa2860d761fef9b1063f3abe20b480c60e4130a
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-08-01 19:20:28 -0600

    added object creation method for to make a notification

--- spec/support/object_creation.rb
+++ spec/support/object_creation.rb
@@ -57,4 +57,14 @@ module ObjectCreation
       default
     )
   end
+
+  def self.create_notification(attributes = {})
+      user = ObjectCreation.create_user
+      product = ObjectCreation.create_product(current_price: '80')
+      ObjectCreation.create_price_log(product: product, price: '100')
+      my_product = MyProduct.create(user: user, product: product)
+      default = {my_product: my_product, discount: 10.0}
+      default.merge!(attributes)
+      MyProductsNotification.create(default)
+  end
 end
