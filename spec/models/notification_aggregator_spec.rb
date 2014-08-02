b526d7130044e51142d08356c4adfe1074debd8b
Evan Tedesco <evantedesco@gmail.com>
2014-08-01 22:47:55 -0600

    it adds all products to the email for a user

--- spec/models/notification_aggregator_spec.rb
+++ spec/models/notification_aggregator_spec.rb
@@ -3,13 +3,16 @@ describe NotificationAggregator do
   it 'aggregates notifications' do
     notification_1 = ObjectCreation.create_notification
     notification_2 = ObjectCreation.create_notification
+    additional_product = MyProduct.create(user: notification_2.user, product: notification_1.my_product.product)
+    MyProductsNotification.create(my_product: additional_product, discount: 10.0)
     ObjectCreation.create_notification(notification_date: 1.day.ago)
 
 
     expect(notification_1.notification_date).to be_nil
     expect(notification_2.notification_date).to be_nil
 
-    expect(NotificationAggregator.aggregate).to eq([[notification_1.user,[notification_1.my_product.product]], [notification_2.user,[notification_2.my_product.product]]])
+    expect(NotificationAggregator.aggregate).to eq([[notification_1.user,[notification_1.my_product.product]],
+                                                    [notification_2.user,[notification_2.my_product.product, additional_product.product]]])
     notification_1.reload
     notification_2.reload
     expect(notification_1.notification_date).to_not be_nil
