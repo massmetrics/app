3cc674b59c615e429726c7333913bae531d29b00
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-08-01 19:13:59 -0600

    fixed seeds file to run once

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-# skus = ["B004EHXKU2", "B000QSNYGI", "B000QSNYGI", "B002HHREU8", "B003V5LXEI"]
-# skus.each do |sku|
-#   Product.create_from_sku(sku)
-# end
-
-
-def make_price_logs(item)
-  [
-    [product: item, created_at: 1.day.ago, price: rand(100000).to_s],
-    [product: item, created_at: 2.day.ago, price: rand(100000).to_s],
-    [product: item, created_at: 3.day.ago, price: rand(100000).to_s],
-    [product: item, created_at: 4.day.ago, price: rand(100000).to_s],
-    [product: item, created_at: 5.day.ago, price: rand(100000).to_s],
-    [product: item, created_at: 6.day.ago, price: rand(100000).to_s],
-    [product: item, created_at: 7.day.ago, price: rand(100000).to_s],
-    [product: item, created_at: 8.day.ago, price: rand(100000).to_s],
-    [product: item, created_at: 9.day.ago, price: rand(100000).to_s]
-  ]
+Product.all.each do |item|
+  item.add_categories(["protein", "protein powder"])
 end
-
-# Product.all.each do |item|
-#   make_price_logs(item).each do |log|
-#     PriceLog.create(log)
-#   end
-# end
-
-# Product.all.each do |item|
-#   item.add_categories(["protein", "protein powder"])
-# end
 
 powders = ["B00ARJN2TK", "B00BEOHFKO", "B000GOY7FO", "B000GOY6Z0", "B002DYJ0OI", "B00FFDSER8", "B002QZORGK", "B000GIPJ0M", "B00HQNNS6C", "B002DYJ0K2", "B002DUD6QU"]
 
@@ -68,3 +33,23 @@ cla = ['B008E77A0O', 'B000Z8Z93K', 'B00EVMVU0W', 'B00HFF23RQ', 'B002DYJ0CU', 'B0
   ProductAdder.add(creatine, ['Creatine'])
 
   ProductAdder.add(cla, ['CLA'])
+
+def make_price_logs(item)
+  [
+    [product: item, created_at: 1.day.ago, price: rand(100000).to_s],
+    [product: item, created_at: 2.day.ago, price: rand(100000).to_s],
+    [product: item, created_at: 3.day.ago, price: rand(100000).to_s],
+    [product: item, created_at: 4.day.ago, price: rand(100000).to_s],
+    [product: item, created_at: 5.day.ago, price: rand(100000).to_s],
+    [product: item, created_at: 6.day.ago, price: rand(100000).to_s],
+    [product: item, created_at: 7.day.ago, price: rand(100000).to_s],
+    [product: item, created_at: 8.day.ago, price: rand(100000).to_s],
+    [product: item, created_at: 9.day.ago, price: rand(100000).to_s]
+  ]
+end
+
+Product.all.each do |item|
+  make_price_logs(item).each do |log|
+    PriceLog.create(log)
+  end
+end
