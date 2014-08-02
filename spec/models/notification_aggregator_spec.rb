58935761f8e6190672216b2e4752e6b120c2b295
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-08-01 22:53:49 -0600

    modified rake task to allow testing

--- spec/models/notification_aggregator_spec.rb
+++ spec/models/notification_aggregator_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 describe NotificationAggregator do
   it 'aggregates notifications' do
     notification_1 = ObjectCreation.create_notification
+    product_1 = notification_1.my_product.product
     notification_2 = ObjectCreation.create_notification
-    additional_product = MyProduct.create(user: notification_2.user, product: notification_1.my_product.product)
+    product_2 = notification_2.my_product.product
+    additional_product = MyProduct.create(user: notification_2.user, product: product_1)
     MyProductsNotification.create(my_product: additional_product, discount: 10.0)
     ObjectCreation.create_notification(notification_date: 1.day.ago)
 
@@ -11,8 +13,10 @@ describe NotificationAggregator do
     expect(notification_1.notification_date).to be_nil
     expect(notification_2.notification_date).to be_nil
 
-    expect(NotificationAggregator.aggregate).to eq([[notification_1.user,[notification_1.my_product.product]],
-                                                    [notification_2.user,[notification_2.my_product.product, additional_product.product]]])
+    expect(NotificationAggregator.aggregate).to eq([
+                                                     [notification_1.user, [product_1]],
+                                                     [notification_2.user, [product_2, product_1]]
+                                                   ])
     notification_1.reload
     notification_2.reload
     expect(notification_1.notification_date).to_not be_nil
