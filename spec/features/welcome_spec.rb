aa2b8ef5759e0a1c4f24cc3d3ffd7e8d835affff
Evan Tedesco <evantedesco@gmail.com>
2014-08-11 21:46:33 -0600

    removed side nave from about page
    Signed-off-by: Steve Magelowitz <steven.magelowitz@gmail.com>

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -4,7 +4,7 @@ feature 'about page' do
   scenario 'user can see about page' do
     visit '/'
     click_link 'About'
-    expect(page).to have_content 'Topics'
+    expect(page).to have_content 'Product sorting'
   end
 end
 
