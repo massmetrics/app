363e19e7129c5cefddf2bc8c67eb205cc20bb3e2
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-12-09 14:09:59 -0800

    Updated category column to name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815232153) do
+ActiveRecord::Schema.define(version: 20141209220612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20140815232153) do
   end
 
   create_table "categories", force: true do |t|
-    t.string "category"
+    t.string "name"
   end
 
   create_table "my_products", force: true do |t|
