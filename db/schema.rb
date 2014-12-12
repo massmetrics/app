df941716c22d3c4621183a972e8f7cc1dcaee824
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-12-12 15:46:25 -0800

    Removed migration that deletes product id from category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813231851) do
+ActiveRecord::Schema.define(version: 20141212230304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20140813231851) do
   end
 
   create_table "categories", force: true do |t|
-    t.string  "category"
+    t.string  "name"
     t.integer "product_id"
   end
 
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 20140813231851) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "product_categories", force: true do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.string  "name"
+  end
+
+  add_index "product_categories", ["category_id"], name: "index_product_categories_on_category_id", using: :btree
+  add_index "product_categories", ["product_id"], name: "index_product_categories_on_product_id", using: :btree
 
   create_table "products", force: true do |t|
     t.string  "sku"
