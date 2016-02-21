23ac37e3d8739a9fd720a42173c83fa20bc3a3f1
Steven Magelowitz <steven.magelowitz@gmail.com>
2016-02-20 16:52:14 -0800

    make category_discounts main entry point

--- spec/models/product_adder_spec.rb
+++ spec/models/product_adder_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe ProductAdder do
   it 'Adds a product and a category at the same time' do
-    VCR.use_cassette('features/admin/submission/add_submission', record: :new_episodes) do |cassette|
+    VCR.use_cassette('features/admin/submission/add_submission') do |cassette|
       new_time = cassette.originally_recorded_at || Time.now
       Timecop.freeze(new_time) do
         ProductAdder.add(['B00ARJN2TK'], ['Protein'])
