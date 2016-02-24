06df5bdeaf16409b8344b87a7f8be4260d56b5b4
Steven Magelowitz <steven.magelowitz@gmail.com>
2016-02-23 21:28:06 -0800

    refactor class methods into self block

--- app/models/notification_aggregator.rb
+++ app/models/notification_aggregator.rb
@@ -1,24 +1,28 @@
 class NotificationAggregator
-  def self.aggregate
-    User.all.map do |user|
-       evaluate(user)
-    end.compact
-  end
+  class << self
+    
+    def aggregate
+      User.all.map do |user|
+        evaluate(user)
+      end.compact
+    end
 
-  def self.evaluate(user)
-    products = []
-    user.notifications.map do |notification|
-      if notification.send_notification?
-        products << notification.my_product.product
-        update_notification(notification)
+    def evaluate(user)
+      products = []
+      user.notifications.map do |notification|
+        if notification.send_notification?
+          products << notification.my_product.product
+          update_notification(notification)
+        end
+      end
+      unless products.empty?
+        [user, products]
       end
     end
-    unless products.empty?
-      [user, products]
-    end
-  end
 
-  def self.update_notification(notification)
-    notification.update(notification_date: Time.now)
+    def update_notification(notification)
+      notification.update(notification_date: Time.now)
+    end
+
   end
 end
